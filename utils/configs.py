43587a67af76226496318c7fd503d00ed5925560
Oli Bridge <ob344@cam.ac.uk>
2024-04-22 12:46:02 +0100

    Script updates

--- utils/configs.py
+++ utils/configs.py
@@ -2,7 +2,6 @@ configs = {
     "vit_4M_pretrain": {
         "image_size": 128,
         "patch_size": 16,
-        # "num_classes": 2, #not used
         "dim": 128,
         "depth": 12,
         "heads": 8,
@@ -10,6 +9,7 @@ configs = {
         "masking_ratio": 0.5,
         "lr": 1e-3,
         "weight_decay": 0.05,
+        "batch_size": 64,
         "schedule_milestones": [50, 85],
         "schedule_gamma": 0.5,
         "epochs": 100,
@@ -17,7 +17,6 @@ configs = {
     "vit_4M_finetune": {
         "image_size": 128,
         "patch_size": 16,
-        # "num_classes": 2, #not used
         "dim": 128,
         "depth": 12,
         "heads": 8,
@@ -27,7 +26,7 @@ configs = {
         "batch_size": 64,
         "schedule_milestones": [70, 90, 95],
         "schedule_gamma": 0.5,
-        "epochs": 100,
+        "epochs": 10,
         "int_lr": 8e-4,
         "int_schedule_milestones": [180, 190],
         "int_schedule_gamma": 0.1,
